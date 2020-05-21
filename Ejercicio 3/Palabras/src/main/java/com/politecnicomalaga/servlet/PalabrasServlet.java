19d6c3358958cb18bfdaf9f2d85c4f4383914965
Isaias Vera <osvaldoisaias.spk@gmail.com>
2020-05-21 17:32:09 +0200

    Ejercicio 3

--- Ejercicio 3/Palabras/src/main/java/com/politecnicomalaga/servlet/PalabrasServlet.java
+++ Ejercicio 3/Palabras/src/main/java/com/politecnicomalaga/servlet/PalabrasServlet.java
@@ -1,12 +1,8 @@
 package com.politecnicomalaga.servlet;
 
 import com.google.gson.Gson;
-import com.google.gson.JsonElement;
 import com.google.gson.reflect.TypeToken;
 import com.politecnicomalaga.Concatenacion;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -14,10 +10,11 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.lang.reflect.Type;
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.logging.Logger;
 import java.util.stream.Collectors;
 
 @WebServlet(
@@ -26,6 +23,7 @@ import java.util.stream.Collectors;
 )
 
 public class PalabrasServlet extends HttpServlet {
+    Logger logger = Logger.getLogger(PalabrasServlet.class.getName());
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String concatenado;
@@ -33,14 +31,18 @@ public class PalabrasServlet extends HttpServlet {
         Concatenacion concatenacion = new Concatenacion();
         Gson gson = new Gson();
         String json = req.getReader().lines().collect(Collectors.joining());
+        logger.info(json);
         try {
             Type collectionType = new TypeToken<Collection<String>>(){}.getType();
             List<String> palabras = gson.fromJson(json,collectionType);
             concatenado = concatenacion.concatenar(palabras);
-            resultado =
+            resultado = "{\"status\":\"ok\",\"palabraA\":\""+palabras.get(0)+"\",\"palabraB\":\""+palabras.get(1)+"\",\"palabraC\":\""+palabras.get(2)+"\",\"concatenado\":\""+concatenado+"\"}";
         } catch (Exception e) {
             resultado = "{\"status\":\"ko\"}";
             e.printStackTrace();
         }
+        PrintWriter out = resp.getWriter();
+        resp.setContentType("application/json");
+        out.print(resultado);
     }
 }
